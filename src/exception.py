9008523cfb0f12d8f3f29c228503d083ace5a69c
YARRAM THIRUPATHIRAO <yarramthirupathirao@gmail.com>
2024-12-03 15:31:30 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -12,7 +12,7 @@ def error_message_detail(error,error_detail:sys):
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message=message_detail(error_message,error_detail=erro_detail)
+        self.error_message=message_detail(error_message,error_detail=error_detail)
         
     def __str__(self):
         return self.error_message
